cd6792d8cf79226fe4f71880b44a31a29b8b4d82
Sabrina Silva <97848666+silvasabrina@users.noreply.github.com>
2023-10-27 23:52:34 -0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -4,12 +4,12 @@ SELECT substring_index(u.nome_completo, ' ', 1) 'Primeiro Nome',
     u.estado 'Localizacao', 
     u.email 'Email' 
 FROM usuarios u
-	JOIN matriculas m ON m.user_id = u.id
-	JOIN cursos c ON c.id = m.curso_id
-	JOIN subcategoria s ON s.id = c.subcategoria_id
-	JOIN categoria ca ON ca.id = s.categoria_id
+    JOIN matriculas m ON m.user_id = u.id
+    JOIN cursos c ON c.id = m.curso_id
+    JOIN subcategoria s ON s.id = c.subcategoria_id
+    JOIN categoria ca ON ca.id = s.categoria_id
 WHERE ca.nome = 'Data Science' 
-	AND s.nome != 'Excel' 
+    AND s.nome != 'Excel' 
     AND datediff(curdate(), u.data_nascimento) > 16 * 365
 GROUP BY u.nome_completo, 
     u.estado, 
