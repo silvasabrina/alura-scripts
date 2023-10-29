5e63cb2e80dcef000e587576141044ef6a110c49
Sabrina Silva <97848666+silvasabrina@users.noreply.github.com>
2023-10-29 18:45:21 -0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,17 +1,17 @@
 USE alura;
 
-SELECT substring_index(u.nome_completo, ' ', 1) 'Primeiro Nome',
-    u.estado 'Localizacao', 
-    u.email 'Email' 
-FROM usuarios u
-    JOIN matriculas m ON m.user_id = u.id
-    JOIN cursos c ON c.id = m.curso_id
-    JOIN subcategoria s ON s.id = c.subcategoria_id
-    JOIN categoria ca ON ca.id = s.categoria_id
-WHERE ca.nome = 'Data Science' 
-    AND s.nome != 'Excel' 
-    AND datediff(curdate(), u.data_nascimento) > 16 * 365
+SELECT substring_index(u.nome_completo, ' ', 1) 'Primeiro Nome', 
+  u.estado 'Localizacao' , 
+  u.email 'Email' 
+FROM usuarios u 
+  JOIN matriculas m ON m.user_id = u.id 
+  JOIN cursos c ON c.id = m.curso_id 
+  JOIN subcategoria s ON s.id = c.subcategoria_id 
+  JOIN categoria ca ON ca.id = s.categoria_id 
+WHERE ca.nome = 'Data Science'
+  AND s.nome != 'Excel' 
+  AND TIMESTAMPDIFF(YEAR, u.data_nascimento, CURDATE()) >=16
 GROUP BY u.nome_completo, 
-    u.estado, 
-    u.email
-HAVING count(*) >= 3;
+  u.estado, 
+  u.email
+HAVING COUNT(*) >= 3;
